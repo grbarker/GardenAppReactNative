5f2c7b5ce964f3401c533499e4a09bab8e288f2e
gblordsburg <bluecoast86@gmail.com>
2018-10-23 16:36:35 -0700

    Add reducers for fetching more of a user's posts.

--- reducers/userposts.js
+++ reducers/userposts.js
@@ -1,8 +1,10 @@
-import { FETCH_USER_POSTS, FETCH_USER_POSTS_SUCCESS, FETCH_USER_POSTS_FAILURE } from '../actions/userposts'
+import { FETCH_USER_POSTS, LESS_USER_POSTS, FETCH_USER_POSTS_SUCCESS, FETCH_USER_POSTS_FAILURE, FETCH_MORE_USER_POSTS_SUCCESS, FETCH_MORE_USER_POSTS_FAILURE } from '../actions/userposts'
 
 const INITIAL_STATE = {
   fetching: false,
   fetched: false,
+  page: null,
+  links: {},
   items: [],
   error: null
 }
@@ -16,17 +18,44 @@ export default function userposts(state = INITIAL_STATE, action) {
       };
     case FETCH_USER_POSTS_SUCCESS:
       return {
-        ...state,
         fetching: false,
         fetched: true,
-        items: action.payload.items
+        page: action.payload._meta.page,
+        links: action.payload._links,
+        items: action.payload.items,
+        error: null
       };
+    case FETCH_MORE_USER_POSTS_SUCCESS:
+      return Object.assign({}, state, {
+        fetching: false,
+        fetched: true,
+        page: action.payload._meta.page,
+        links: action.payload._links,
+        items: state.items.concat(action.payload.items),
+        error: null
+      });
     case FETCH_USER_POSTS_FAILURE:
       return {
         ...state,
         fetching: false,
         fetched: false,
-        error: action.payload.error
+        error: action.payload
+      };
+    case FETCH_MORE_USER_POSTS_FAILURE:
+      return Object.assign({}, state, {
+        fetching: false,
+        fetched: false,
+        error: action.payload
+      });
+    case LESS_USER_POSTS:
+      return {
+        ...state,
+        links: {
+          next: "/api/user/posts?per_page=10&page=2",
+          prev: null,
+          self: "/api/user/posts?per_page=10&page=1",
+        },
+        items: state.items.splice(0, 10)
       };
     default :
       return state
