4af3873967216afcd81a279d841547f31efd01e4
gblordsburg <bluecoast86@gmail.com>
2018-10-23 15:53:41 -0700

    Add actions to fetch a user's intitial set of plants, to get the next batch of plants from the api endpoint, and to show less plants(which reverts the shown plants back to the initial set).

--- actions/userplants.js
+++ actions/userplants.js
@@ -1,22 +1,33 @@
 import axios from 'axios';
 
 //Get category
-export const FETCH_USER_PLANTS = 'FETCH_PLANTS';
-export const FETCH_USER_PLANTS_SUCCESS = 'FETCH_PLANTS_SUCCESS';
-export const FETCH_USER_PLANTS_FAILURE = 'FETCH_PLANTS_FAILURE';
+export const FETCH_USER_PLANTS = 'FETCH_USER_PLANTS';
+export const FETCH_USER_PLANTS_SUCCESS = 'FETCH_USER_PLANTS_SUCCESS';
+export const FETCH_USER_PLANTS_FAILURE = 'FETCH_USER_PLANTS_FAILURE';
+export const FETCH_MORE_USER_PLANTS_SUCCESS = 'FETCH_MORE_USER_PLANTS_SUCCESS';
+export const LESS_USER_PLANTS = 'LESS_USER_PLANTS';
 
-const api = "http://34.221.120.52/api/plants/"
+const api = "http://34.221.120.52/api/user/plants/"
 
-export function getUserPlants(token) {
-  const request = axios({
-    method: 'get',
-    url: `${api}`,
-    headers: {Authorization: `Bearer ${token}`}
-  });
+export function getUserPlants(dispatch, token, uri) {
+  var uri = 'http://34.221.120.52' + uri
+  return function (dispatch)  {
+    console.log("Trying to DEBUG this get request for the next set of plants!!!", token, ", ", uri)
+    axios.get(uri, {
+      headers: {Authorization: `Bearer ${token}`}
+    })
+    .then((response) => {
+      dispatch(getMoreUserPlantsSuccess(response.data)) && console.log(response.data)
+    })
+    .catch(error => {
+       dispatch(getMoreUserPlantsFailure(error.response.data)) && console.log(error.response.data.error)
+    })
+    };
+}
 
+export function lessUserPlants() {
   return {
-    type: FETCH_USER_PLANTS,
-    payload: request
+    type: LESS_USER_PLANTS,
   };
 }
 
@@ -30,6 +41,20 @@ export function getUserPlantsSuccess(data) {
 export function getUserPlantsFailure(data) {
   return {
     type: FETCH_USER_PLANTS_FAILURE,
+    payload: data.error
+  };
+}
+
+export function getMoreUserPlantsSuccess(data) {
+  return {
+    type: FETCH_MORE_USER_PLANTS_SUCCESS,
+    payload: data
+  };
+}
+
+export function getMoreUserPlantsFailure(data) {
+  return {
+    type: FETCH_MORE_USER_PLANTS_FAILURE,
     payload: data.error
   };
 }
