e3dd5dbf0c7b8858d0ae2e563383151a87958f43
gblordsburg <bluecoast86@gmail.com>
2018-10-23 15:50:19 -0700

    Add showFolowers and hideFollowers actions.

--- actions/followers.js
+++ actions/followers.js
@@ -6,6 +6,8 @@ export const FETCH_FOLLOWERS_SUCCESS = 'FETCH_FOLLOWERS_SUCCESS';
 export const FETCH_FOLLOWERS_FAILURE = 'FETCH_FOLLOWERS_FAILURE';
 export const FETCH_MORE_FOLLOWERS_SUCCESS = 'FETCH_MORE_FOLLOWERS_SUCCESS';
 export const LESS_FOLLOWERS = 'LESS_FOLLOWERS';
+export const SHOW_FOLLOWERS = 'SHOW_FOLLOWERS';
+export const HIDE_FOLLOWERS = 'HIDE_FOLLOWERS';
 
 const api = "http://34.221.120.52/api/user/followers/"
 
@@ -30,6 +32,19 @@ export function lessFollowers() {
     type: LESS_FOLLOWERS,
   };
 }
+
+export function showFollowers() {
+  return {
+    type: SHOW_FOLLOWERS,
+  };
+}
+
+export function hideFollowers() {
+  return {
+    type: HIDE_FOLLOWERS,
+  };
+}
+
 
 export function getFollowersSuccess(data) {
   return {
