c45abbae315e807918e70583444c5377cdb70008
gblordsburg <bluecoast86@gmail.com>
2018-10-23 16:39:25 -0700

    Add followers, followed, and locations to rootReducer.

--- reducers/index.js
+++ reducers/index.js
@@ -4,17 +4,21 @@ import user from './user.js'
 import posts from './posts.js';
 import plants from './plants.js';
 import followers from './followers.js';
+import followed from './followed.js';
 import userposts from './userposts.js';
 import userplants from './userplants.js';
+import locations from './locations.js';
 
 const rootReducer = combineReducers({
   auth, //<--Login
   user, //<--User
   posts, //<--Posts
   plants, //<--Plants
+  followers, //<--Followers
+  followed, //<--Followed
   userposts, //<--UserPosts
   userplants, //<--UserPlants
-  followers, //<--Followers
+  locations, //<--Locations
 });
 
 export default rootReducer;
