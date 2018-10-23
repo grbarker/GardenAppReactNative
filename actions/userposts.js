1444756a958d7eb7dd53822eefe7196b2ffe97a0
gblordsburg <bluecoast86@gmail.com>
2018-10-23 15:55:41 -0700

    Add the same actions as the previous commit, but for a user's posts instead. Fetch the initial batch of a user's posts, get the next batch, and show less(revert back to showing only the initial batch of posts.

--- actions/userposts.js
+++ actions/userposts.js
@@ -4,19 +4,30 @@ import axios from 'axios';
 export const FETCH_USER_POSTS = 'FETCH_USER_POSTS';
 export const FETCH_USER_POSTS_SUCCESS = 'FETCH_USER_POSTS_SUCCESS';
 export const FETCH_USER_POSTS_FAILURE = 'FETCH_USER_POSTS_FAILURE';
+export const FETCH_MORE_USER_POSTS_SUCCESS = 'FETCH_MORE_USER_POSTS_SUCCESS';
+export const LESS_USER_POSTS = 'LESS_USER_POSTS';
 
 const api = "http://34.221.120.52/api/posts/"
 
-export function getUserPosts(token) {
-  const request = axios({
-    method: 'get',
-    url: `${api}`,
-    headers: {Authorization: `Bearer ${token}`}
-  });
+export function getUserPosts(dispatch, token, uri) {
+  var uri = 'http://34.221.120.52' + uri
+  return function (dispatch)  {
+    console.log("Trying to DEBUG this get request for the next set of posts!!!", token, ", ", uri)
+    axios.get(uri, {
+      headers: {Authorization: `Bearer ${token}`}
+    })
+    .then((response) => {
+      dispatch(getMoreUserPostsSuccess(response.data)) && console.log(response.data)
+    })
+    .catch(error => {
+       dispatch(getMoreUserPostsFailure(error.response.data)) && console.log(error.response.data.error)
+    })
+    };
+}
 
+export function lessUserPosts() {
   return {
-    type: FETCH_USER_POSTS,
-    payload: request
+    type: LESS_USER_POSTS,
   };
 }
 
@@ -30,6 +41,20 @@ export function getUserPostsSuccess(data) {
 export function getUserPostsFailure(data) {
   return {
     type: FETCH_USER_POSTS_FAILURE,
+    payload: data.error
+  };
+}
+
+export function getMoreUserPostsSuccess(data) {
+  return {
+    type: FETCH_MORE_USER_POSTS_SUCCESS,
+    payload: data
+  };
+}
+
+export function getMoreUserPostsFailure(data) {
+  return {
+    type: FETCH_MORE_USER_POSTS_FAILURE,
     payload: data.error
   };
 }
