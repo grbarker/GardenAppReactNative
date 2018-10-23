12fc1c590f5dc5bafe73550b9d93897571d3eb48
gblordsburg <bluecoast86@gmail.com>
2018-10-23 16:38:25 -0700

    Add reducers for fetching more or less of a user's plants.

--- reducers/userplants.js
+++ reducers/userplants.js
@@ -1,8 +1,14 @@
-import { FETCH_USER_PLANTS, FETCH_USER_PLANTS_SUCCESS, FETCH_USER_PLANTS_FAILURE } from '../actions/userplants'
+import {
+  FETCH_USER_PLANTS, LESS_USER_PLANTS, FETCH_USER_PLANTS_SUCCESS,
+  FETCH_USER_PLANTS_FAILURE, FETCH_MORE_USER_PLANTS_SUCCESS,
+  FETCH_MORE_USER_PLANTS_FAILURE
+} from '../actions/userplants'
 
 const INITIAL_STATE = {
   fetching: false,
   fetched: false,
+  page: null,
+  links: {},
   items: [],
   error: null
 }
@@ -16,17 +22,44 @@ export default function userplants(state = INITIAL_STATE, action) {
       };
     case FETCH_USER_PLANTS_SUCCESS:
       return {
-        ...state,
         fetching: false,
         fetched: true,
-        items: action.payload.items
+        page: action.payload._meta.page,
+        links: action.payload._links,
+        items: action.payload.items,
+        error: null
       };
+    case FETCH_MORE_USER_PLANTS_SUCCESS:
+      return Object.assign({}, state, {
+        fetching: false,
+        fetched: true,
+        page: action.payload._meta.page,
+        links: action.payload._links,
+        items: state.items.concat(action.payload.items),
+        error: null
+      });
     case FETCH_USER_PLANTS_FAILURE:
       return {
         ...state,
         fetching: false,
         fetched: false,
-        error: action.payload.error
+        error: action.payload
+      };
+    case FETCH_MORE_USER_PLANTS_FAILURE:
+      return Object.assign({}, state, {
+        fetching: false,
+        fetched: false,
+        error: action.payload
+      });
+    case LESS_USER_PLANTS:
+      return {
+        ...state,
+        links: {
+          next: "/api/user/plants?per_page=10&page=2",
+          prev: null,
+          self: "/api/user/plants?per_page=10&page=1",
+        },
+        items: state.items.splice(0, 10)
       };
     default :
       return state
