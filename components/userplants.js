3c2fe22ea777f2c203f956b7e5390b17f1407fd9
gblordsburg <bluecoast86@gmail.com>
2018-11-07 13:03:52 -0800

    A styling to component to fit the garden/green theme. Edit TextButton to AlteredTextButton.

--- components/userplants.js
+++ components/userplants.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 import { ScrollView, View, Text, TouchableOpacity, StyleSheet, Button } from 'react-native'
-import TextButton from './TextButton'
+import AlteredTextButton from './AlteredTextButton'
 import Moment from 'react-moment';
 import 'moment-timezone';
 import { connect } from 'react-redux'
 import axios from 'axios';
-import { white, my_green, green, gray, red, purple, orange, blue, my_blue, lightPurp, black, pink } from '../utils/colors'
+import {
+  white, my_green, green, gray4, red, purple, orange, blue, my_blue,
+  lightPurp, black, pink
+} from '../utils/colors'
 import {
   getUserPlants, lessUserPlants, getUserPlantsSuccess, getUserPlantsFailure,
   getMoreUserPlantsSuccess, getMoreUserPlantsFailure
@@ -24,6 +27,10 @@ class UserPlants extends Component {
     const { dispatch } = this.props
     console.log("Dispatching lessUserPlants")
     dispatch(lessUserPlants())
+  }
+
+  inactiveButton = () => {
+    var inactiveButtonPressed = true
   }
 
   showState = () => {
@@ -54,7 +61,7 @@ class UserPlants extends Component {
   render() {
     const {  links, userplant_items, fetching, fetched_userplants, token, error, state, page } = this.props
     //TRYING TO SET UP A 'NEXT' Button
-    //TRYING TO PASS THE 'NEXT' LINK DOWN TO THE TextButton
+    //TRYING TO PASS THE 'NEXT' LINK DOWN TO THE AlteredTextButton
     //AND THEN FIGURE OUT HOW TO dispatch getUsers
     //console.log("Here's the token!.....", token)
     //console.log("Fetching the next set of userplants.")
@@ -68,7 +75,10 @@ class UserPlants extends Component {
       //console.log(state)
       //console.log("Trying to get the uri.....", uri)
       return (
-        <ScrollView>
+        <ScrollView style={styles.scrollViewAsContainer}>
+          <View style = {styles.scrollViewHeaderContainer}>
+            <Text style = {styles.scrollViewHeaderText}>Here are you're most recent plants</Text>
+          </View>
           <View>
             {userplant_items.map((userplant_item, index) => (
               <View key = {userplant_item.id} style = {styles.container}>
@@ -79,15 +89,41 @@ class UserPlants extends Component {
               </View>
             ))}
           </View>
-          {(links.next) ?
-            <TextButton style={{margin: 20}} onPress={e => this.nextUserPlants(token, uri)}>
-              More Plants
-            </TextButton>
-            : null
-          }
-          <TextButton style={{margin: 20}} onPress={e => this.lessUserPlants()}>
-            Less Plants
-          </TextButton>
+          <View style={styles.moreLessButtonsContainer}>
+            {(links.prev) ?
+              <AlteredTextButton
+                style={styles.filledTextButton}
+                textStyle={styles.whiteText}
+                onPress={e => this.lessUserPlants()}
+              >
+                Less Plants
+              </AlteredTextButton>
+              :
+                <AlteredTextButton
+                  style={styles.inactiveFilledTextButton}
+                  textStyle={styles.whiteText}
+                  onPress={this.inactiveButton}>
+                  Less Plants
+                </AlteredTextButton>
+            }
+            {(links.next) ?
+              <AlteredTextButton
+                style={styles.filledTextButton}
+                textStyle={styles.whiteText}
+                onPress={e => this.nextUserPlants(token, uri)}
+              >
+                More Plants
+              </AlteredTextButton>
+              :
+                <AlteredTextButton
+                  style={styles.inactiveFilledTextButton}
+                  textStyle={styles.whiteText}
+                  onPress={this.inactiveButton}
+                >
+                  More Plants
+                </AlteredTextButton>
+            }
+          </View>
         </ScrollView>
       )
     } else if (error) {
@@ -122,21 +158,78 @@ const mapStateToProps = (state, ownProps) => {
 export default connect(mapStateToProps)(UserPlants);
 
 const styles = StyleSheet.create ({
-   container: {
-      padding: 5,
-      marginTop: 3,
-      backgroundColor: '#f0f4f0',
-      alignItems: 'center',
-   },
-   errorContainer: {
-      padding: 5,
-      marginTop: 3,
-      marginBottom: 30,
-      backgroundColor: '#d9f9b1',
-      alignItems: 'center',
-   },
-   text: {
-     fontSize: 20,
-      color: black
-   }
+  scrollViewAsContainer: {
+    borderWidth: 2,
+    borderRadius: 3,
+    borderColor: my_green,
+    marginTop: 3,
+  },
+  container: {
+    padding: 5,
+    marginTop: 3,
+    backgroundColor: '#f0f4f0',
+    alignItems: 'center',
+  },
+  scrollViewHeaderContainer: {
+    backgroundColor: my_green,
+  },
+  moreLessButtonsContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    padding: 5,
+    margin: 3,
+    backgroundColor: '#f0f4f0',
+  },
+  errorContainer: {
+    padding: 5,
+    marginTop: 3,
+    marginBottom: 30,
+    backgroundColor: '#d9f9b1',
+    alignItems: 'center',
+  },
+  textButton: {
+    padding: 5,
+    color: my_green,
+    borderColor: my_green,
+    borderWidth: 2,
+    borderRadius: 5
+  },
+  inactiveTextButton: {
+    padding: 5,
+    borderColor: gray4,
+    borderWidth: 2,
+    borderRadius: 5
+  },
+  filledTextButton: {
+    padding: 5,
+    backgroundColor: my_green,
+    borderColor: my_green,
+    borderWidth: 2,
+    borderRadius: 5
+  },
+  inactiveFilledTextButton: {
+    padding: 5,
+    backgroundColor: gray4,
+    borderColor: gray4,
+    borderWidth: 2,
+    borderRadius: 5
+  },
+  text: {
+    fontSize: 20,
+    color: black
+  },
+  whiteText: {
+    fontSize: 16,
+    color: white
+  },
+  myGreenText: {
+    fontSize: 16,
+    color: my_green
+  },
+  gray4Text: {
+    fontSize: 16,
+    color: gray4
+  }
 })
