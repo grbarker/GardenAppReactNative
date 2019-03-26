21ba695848a78da9cfa3fbcf5b49e714973e09ca
gblordsburg <bluecoast86@gmail.com>
2019-03-26 13:19:51 -0700

    Add functionality to show the state of the field. Will have to credit where it was pulled form later.

--- components/renderField.js
+++ components/renderField.js
@@ -2,8 +2,11 @@ import React, { Component, PropTypes } from 'react';
 import { ScrollView, View, Text, TextInput, TouchableOpacity, StyleSheet, Button } from 'react-native'
 import { red, orange } from '../utils/colors'
 
+const formStates = ['active', 'autofilled', 'asyncValidating', 'dirty', 'invalid', 'pristine',
+    'submitting', 'touched', 'valid', 'visited'];
 
-const renderField = ({ input, label, type, placeholder, style, meta: { touched, error, invalid, warning } }) => (
+const renderField = ({ input, label, type, placeholder, style, meta, meta: { visited, touched, error, invalid, warning, dirty, pristine } }) => (
+
   <View>
     <TextInput {...input}
         style={style}
@@ -13,7 +16,15 @@ const renderField = ({ input, label, type, placeholder, style, meta: { touched, 
         keyboardType='default'
         placeholder={placeholder}
         value={input.value}
-        onChangeText={input.onChange} />
+        onChangeText={input.onChange}
+        onBlur={input.onBlur}
+        onFocus={input.onFocus} />
+    <Text>The { input.name} input is:</Text>
+    {
+      formStates.filter((state) => meta[state]).map((state) => {
+        return <Text key={state}> - { state }</Text>;
+      })
+    }
       {touched && (
         (error && <Text style={{ fontSize: 20, color: red, }}>{error}</Text>) ||
         (warning && <Text style={{ fontSize: 20, color: orange }}>{warning}</Text>)
