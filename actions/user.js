57008d0513feea11150e19851f60de011d21956e
gblordsburg <bluecoast86@gmail.com>
2019-03-28 16:05:01 -0700

    Add action creators and reducers to handle getting another user besides the logged in user and switching which should be shown on the profile page.

--- actions/user.js
+++ actions/user.js
@@ -4,6 +4,10 @@ import axios from 'axios';
 export const FETCH_USER = 'FETCH_USER';
 export const FETCH_USER_SUCCESS = 'FETCH_USER_SUCCESS';
 export const FETCH_USER_FAILURE = 'FETCH_USER_FAILURE';
+export const FETCH_OTHER_USER_SUCCESS = 'FETCH_OTHER_USER_SUCCESS';
+export const FETCH_OTHER_USER_FAILURE = 'FETCH_OTHER_USER_FAILURE';
+export const SET_OTHER_USER = 'SET_OTHER_USER';
+export const SET_CURRENT_USER = 'SET_CURRENT_USER';
 
 const api = "http://34.221.120.52/api/user"
 
@@ -32,4 +36,31 @@ export function getUserFailure(data) {
     type: FETCH_USER_FAILURE,
     payload: data.error
   };
+}
+
+export function getOtherUserSuccess(data) {
+  return {
+    type: FETCH_OTHER_USER_SUCCESS,
+    payload: data
+  };
+}
+
+export function getOtherUserFailure(data) {
+  return {
+    type: FETCH_OTHER_USER_FAILURE,
+    payload: data.error
+  };
+}
+
+export function setCurrentUser() {
+  return {
+    type: SET_CURRENT_USER,
+  }
+}
+
+export function setOtherUser(id) {
+  return {
+    type: SET_OTHER_USER,
+    payload: id
+  }
 }
--- reducers/user.js
+++ reducers/user.js
@@ -1,13 +1,39 @@
-import { FETCH_USER, FETCH_USER_SUCCESS, FETCH_USER_FAILURE } from '../actions/user'
+import {
+  FETCH_USER, FETCH_USER_SUCCESS, FETCH_USER_FAILURE,
+  FETCH_OTHER_USER, FETCH_OTHER_USER_SUCCESS, FETCH_OTHER_USER_FAILURE,
+  SET_CURRENT_USER, SET_OTHER_USER
+} from '../actions/user'
 
 const INITIAL_STATE = {
   fetching: false,
   fetched: false,
   user: {},
-  error: null
+  error: null,
+  showCurrentUser: false,
+  otherUserBool: false,
+  otherUserID: null,
+  otherFetching: false,
+  otherFetched: false,
+  otherUser: {},
+  otherError: null
 }
 export default function user(state = INITIAL_STATE, action) {
   switch (action.type) {
+    case SET_CURRENT_USER:
+      return {
+        ...state,
+        showCurrentUser: true,
+        otherUserBool: false,
+        otherFetched: false,
+      };
+    case SET_OTHER_USER:
+      return {
+        ...state,
+        fetched: false,
+        showCurrentUser: false,
+        otherUserBool: true,
+        otherUserID: action.payload
+      };
     case FETCH_USER:
       return {
         ...state,
@@ -27,6 +53,26 @@ export default function user(state = INITIAL_STATE, action) {
         fetching: false,
         fetched: false,
         error: action.payload.error
+      };
+    case FETCH_OTHER_USER:
+      return {
+        ...state,
+        otherFetching: true,
+        otherFetched: false,
+      };
+    case FETCH_OTHER_USER_SUCCESS:
+      return {
+        ...state,
+        otherFetching: false,
+        otherFetched: true,
+        otherUser: action.payload
+      };
+    case FETCH_OTHER_USER_FAILURE:
+      return {
+        ...state,
+        otherFetching: false,
+        otherFetched: false,
+        otherError: action.payload.error
       };
     default :
       return state
