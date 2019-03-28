f123d0f63a99eb85845ca23959490547cfdf5602
gblordsburg <bluecoast86@gmail.com>
2019-03-28 15:58:00 -0700

    Add action creators to handle the success or failure of an api call in the associated component. Edit action creator, but it was replaced by moving the api call to the component where it belongs.

--- actions/plants.js
+++ actions/plants.js
@@ -4,23 +4,27 @@ import axios from 'axios';
 export const FETCH_PLANTS = 'FETCH_PLANTS';
 export const FETCH_PLANTS_SUCCESS = 'FETCH_PLANTS_SUCCESS';
 export const FETCH_PLANTS_FAILURE = 'FETCH_PLANTS_FAILURE';
+export const FETCH_MORE_PLANTS_SUCCESS = 'FETCH_PLANTS_SUCCESS';
+export const FETCH_MORE_PLANTS_FAILURE = 'FETCH_PLANTS_FAILURE';
 export const LESS_PLANTS = 'LESS_PLANTS';
 
 const api = "http://34.221.120.52/api/plants/"
 
 export function getPlants(dispatch, token, uri) {
-  var uri = 'http://34.221.120.52' + uri
+  var uri_end = uri_end
+  var uri = 'http://34.221.120.52' + uri_end
   return function (dispatch)  {
-    console.log("Trying to DEBUG this get request for the next set of posts!!!", token, ", ", uri)
+    console.log("Trying to DEBUG this get request for the next set of plants!!!", token, ", ", uri)
     axios.get(uri, {
       headers: {Authorization: `Bearer ${token}`}
     })
     .then((response) => {
-      console.log(reponse)
-      dispatch(getPlantsSuccess(response.data)) && console.log(response.data)
+      (uri_end === '/api/plants')
+      ? dispatch(getPlantsSuccess(response.data)) && console.log(response.data, 'GETTING FIRST SET OF PLANTS SUCCESS')
+      : (dispatch(getMorePlantsSuccess(response.data)) && console.log('GETTING MORE PLANTS'))
     })
     .catch(error => {
-       dispatch(getMorePlantsFailure(error.response.data)) && console.log(error.response.data.error)
+       dispatch(getMorePlantsFailure(error)) && console.log(error)
     })
     };
 }
@@ -41,6 +45,20 @@ export function getPlantsSuccess(data) {
 export function getPlantsFailure(data) {
   return {
     type: FETCH_PLANTS_FAILURE,
+    payload: data.error
+  };
+}
+
+export function getMorePlantsSuccess(data) {
+  return {
+    type: FETCH_MORE_PLANTS_SUCCESS,
+    payload: data
+  };
+}
+
+export function getMorePlantsFailure(data) {
+  return {
+    type: FETCH_MORE_PLANTS_FAILURE,
     payload: data.error
   };
 }
